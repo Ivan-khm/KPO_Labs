e057d660028ee50225c2616eb7c42fc3db38a15f
Ivan-khm <55186708+Ivan-khm@users.noreply.github.com>
2019-09-28 00:40:50 +0800

    Add files via upload

---
+++ Lab_3/Lvl_3-4/client.py
@@ -0,0 +1,14 @@
+import socket
+import sys
+
+sock = socket.socket()
+sock.connect(('localhost', 8888))
+while True:
+    mes = input("Введите сообщение: ")
+    if mes == 'exit':
+        sock.send(mes.encode())
+        sock.close()
+        sys.exit()
+    else:
+        sock.send(mes.encode())
+        print(sock.recv(1024).decode())
---
+++ Lab_3/Lvl_3-4/server.py
@@ -0,0 +1,36 @@
+import random
+import socket
+
+
+def open_file():
+    try:
+        file = open('Shakespeare.txt', 'r')
+        list_sonnets = file.read().split('+')
+        print(list_sonnets)
+        file.close()
+        return list_sonnets
+    except:
+        print("Ошибка!")
+
+
+def rand_son():
+    op = open_file()
+    random_sonnets = random.choice(op)
+    return random_sonnets
+
+
+sock = socket.socket()
+sock.bind(('', 8888))
+sock.listen(15)
+connect, addr = sock.accept()
+
+print('Подключен:', addr)
+
+while True:
+    data = connect.recv(1024)
+    if not data:
+        break
+    else:
+        sonnet = rand_son()
+        connect.send(sonnet.encode())
+connect.close()
