fa65519c0463e5a1dd6b421168cd3d4ccd28a9c2
Ivan-khm <55186708+Ivan-khm@users.noreply.github.com>
2019-10-05 02:21:04 +0800

    Add files via upload

---
+++ Lab_4/CreateTb.py
@@ -0,0 +1,62 @@
+class CreateTables:
+    # Функция создания таблиц БД
+    def create_table(self, connection, curs):
+        # Создание таблицы предметов
+        curs.execute("""CREATE TABLE subject
+                            (id_subject INTEGER PRIMARY KEY, subject_name text, day_of_the_week text,
+                             cabinet text, teacher text)
+                         """)
+
+        # Создание таблицы преподавателей
+        curs.execute("""CREATE TABLE teachers
+                                (id_teacher INTEGER PRIMARY KEY, full_name text, teach_subject text,
+                                 count_subject_per_week text, count_student_subject text)
+                             """)
+
+        # Создание таблицы дней недели
+        curs.execute("""CREATE TABLE days_week
+                                    (id_days_week INTEGER PRIMARY KEY, day_name text, count_subj_per_day text)
+                                 """)
+
+        # Сохранение изменений
+        connection.commit()
+
+    # Функция добавления данных в таблицы БД
+    def adding_data(self, connection, curs):
+        # Данные для таблицы предметов
+        data_sub = [('1', 'KPO', 'Saturday', 'D-614', 'Kurganskaya O.V.'),
+                    ('2', 'KPO', 'Saturday', 'D-608', 'Kurganskaya O.V.'),
+                    ('3', 'TPR', 'Wednesday', 'D-313', 'Ermakov A.A.'),
+                    ('4', 'NPO', 'Thursday', 'D-218', 'Ermakov A.A.'),
+                    ('5', 'ZPS', 'Tuesday', 'D-523', 'Krakovskii Y.M.'),
+                    ('6', 'ZPS', 'Friday', 'A-208', 'Krakovskii Y.M.'),
+                    ('7', 'TPR', 'Tuesday', 'A-511', 'Ermakov A.A.'),
+                    ('8', 'OUD', 'Friday', 'D-618', 'Kirillova T.K.'),
+                    ('9', 'AVS', 'Tuesday', 'D-614', 'Kashkavskii V.V.')]
+
+        # Добавление данных в таблицу предметов
+        curs.executemany("INSERT INTO subject VALUES (?, ?, ?, ?, ?)", data_sub)
+
+        # Данные для таблицы преподавателей
+        data_teach = [('1', 'Kurganskaya O.V.', 'KPO', '2', '18'),
+                      ('2', 'Ermakov A.A.', 'TPR', '3', '14'),
+                      ('3', 'Ermakov A.A.', 'NPO', '4', '12'),
+                      ('4', 'Krakovskii Y.M.', 'ZPS', '1', '17'),
+                      ('5', 'Kashkavskii V.V.', 'AVS', '2', '16')]
+
+        # Добавление данных в таблицу преподавателей
+        curs.executemany("INSERT INTO teachers VALUES (?, ?, ?, ?, ?)", data_teach)
+
+        # Данные для таблицы дней недели
+        data_day_week = [('1', 'Monday', '0'),
+                         ('2', 'Tuesday', '3'),
+                         ('3', 'Wednesday', '1'),
+                         ('4', 'Thursday', '1'),
+                         ('5', 'Friday', '2'),
+                         ('6', 'Saturday', '2')]
+
+        # Добавление данных в таблицу дней недели
+        curs.executemany("INSERT INTO days_week VALUES (?, ?, ?)", data_day_week)
+
+        # Сохранение изменений
+        connection.commit()
---
+++ Lab_4/GetInf.py
@@ -0,0 +1,29 @@
+
+class GetInf:
+    # Получение данных из БД по введенным данным первого запроса
+    def get_information_first_query(self, day, numb_cabinet, curs):
+        sql = "SELECT full_name, teach_subject, count_subject_per_week, count_student_subject" \
+              " FROM teachers, subject WHERE subject.day_of_the_week=? AND" \
+              " subject.cabinet=? AND teachers.full_name = subject.teacher"
+        curs.execute(sql, ["%s" % day, "%s" % numb_cabinet])
+        return curs.fetchall()
+
+    # Получение данных из БД по введенным данным второго запроса
+    def get_information_second_query(self, day, curs):
+        sql = "SELECT full_name, teach_subject, count_subject_per_week, count_student_subject" \
+              " FROM teachers, subject WHERE NOT (subject.day_of_the_week=?) AND" \
+              " (teachers.full_name = subject.teacher)"
+        curs.execute(sql, ["%s" % day])
+        return curs.fetchall()
+
+    # Получение данных из БД по введенным данным третьего запроса
+    def get_information_third_query(self, count, curs):
+        sql = "SELECT day_name FROM days_week WHERE count_subj_per_day=?"
+        curs.execute(sql, ["%s" % count])
+        return curs.fetchall()
+
+    # Получение данных из БД по введенным данным четвертого запроса
+    def get_information_fourth_query(self, count, curs):
+        sql = "SELECT day_name FROM days_week WHERE count_subj_per_day=?"
+        curs.execute(sql, ["%s" % count])
+        return curs.fetchall()
