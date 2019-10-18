34c560bb550b042d0c956b8f5621f194da4c754c
Ivan-khm <55186708+Ivan-khm@users.noreply.github.com>
2019-10-19 02:56:29 +0800

    Запуск маршаллизации/демаршаллизации

--- Lab_6/Lvl_6-2/main.py
+++ Lab_6/Lvl_6-2/main.py
@@ -5,10 +5,16 @@ from UnMarshal import UnMarshal
 if __name__ == "__main__":
 
     # Маршаллизация
+    print("Маршаллизация:")
+    # Создание объекта класса 'CreateXml'
     cx = CreateXml()
+    # Вызов функции 'create_xml' класса 'CreateXml'
     cx.create_xml()
+    print("Успешно!\n")
 
     # Демаршаллизация
     print("Демаршаллизация:\n")
+    # Создание объекта класса 'UnMarshal'
     um = UnMarshal()
+    # Вызов функции 'unmarshal' класса 'UnMarshal'
     um.unmarshal('created_xml.xml')
