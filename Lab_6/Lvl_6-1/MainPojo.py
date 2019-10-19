d1a9b1dd565bf57504752e75f26a45ea5692045f
Ivan-khm <55186708+Ivan-khm@users.noreply.github.com>
2019-10-19 09:02:55 +0800

    Основной класс Pojo

--- Lab_6/Lvl_6-1/MainPojo.py
+++ Lab_6/Lvl_6-1/MainPojo.py
@@ -3,11 +3,13 @@ class MainPojo(object):
     # Объявление переменных
     def __init__(self):
         self.Timetable = ''
-
+    
+    # Функция, возвращающая значение тега 'Timetable'
     @property
     def get_Timetable(self):
         return self.Timetable
-
+   
+    # Функция присваивания значения тегу 'Timetable'
     @get_Timetable.setter
     def get_Timetable(self, value):
         self.Timetable = value
