4a94b2f0161465ee175a8cb0c62dc5773eee4ee9
Ivan-khm <55186708+Ivan-khm@users.noreply.github.com>
2019-10-19 09:12:34 +0800

    Дочерний объект Pojo

--- Lab_6/Lvl_6-1/Subject.py
+++ Lab_6/Lvl_6-1/Subject.py
@@ -6,26 +6,32 @@ class Subject(object):
         self.Day = ''
         self.Audience = ''
 
+    # Функция, возвращающая значение тега 'name_sub'
     @property
     def get_name_sub(self):
         return self.name_sub
 
+    # Функция присваивания значения тегу 'name_sub'
     @get_name_sub.setter
     def get_name_sub(self, value):
         self.name_sub = value
 
+    # Функция, возвращающая значение тега 'Day'
     @property
     def get_day(self):
         return self.Day
 
+    # Функция присваивания значения тегу 'Day'
     @get_day.setter
     def get_day(self, value):
         self.Day = value
 
+    # Функция, возвращающая значение тега 'Audience'
     @property
     def get_audience(self):
         return self.Audience
 
+    # Функция присваивания значения тегу 'Audience'
     @get_audience.setter
     def get_audience(self, value):
         self.Audience = value
@@ -33,4 +39,4 @@ class Subject(object):
     # Вывод в консоль полученных данных
     def toStringSubject(self):
         print("[name_sub = " + self.name_sub + ", Day = " + self.Day + ", Audience = " + self.Audience + "]")
- 
+  
