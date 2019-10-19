24ea209aeade031e7bf2aa4f115cf3c3a084fdd8
Ivan-khm <55186708+Ivan-khm@users.noreply.github.com>
2019-10-19 09:19:50 +0800

    Дочерний объект Pojo

--- Lab_6/Lvl_6-1/Teacher.py
+++ Lab_6/Lvl_6-1/Teacher.py
@@ -7,34 +7,42 @@ class Teacher(object):
         self.Count_sub_per_week = ''
         self.Count_students = ''
 
+    # Функция, возвращающая значение тега 'full_name'
     @property
     def get_full_name(self):
         return self.full_name
 
+    # Функция присваивания значения тегу 'full_name'
     @get_full_name.setter
     def get_full_name(self, value):
         self.full_name = value
 
+    # Функция, возвращающая значение тега 'Sub_teach'
     @property
     def get_sub_teach(self):
         return self.Sub_teach
 
+    # Функция присваивания значения тегу 'Sub_teach'
     @get_sub_teach.setter
     def get_sub_teach(self, value):
         self.Sub_teach = value
 
+    # Функция, возвращающая значение тега 'Count_sub_per_week''
     @property
     def get_count_sub_per_week(self):
         return self.Count_sub_per_week
 
+    # Функция присваивания значения тегу 'Count_sub_per_week'
     @get_count_sub_per_week.setter
     def get_count_sub_per_week(self, value):
         self.Count_sub_per_week = value
 
+    # Функция, возвращающая значение тега 'Count_students'
     @property
     def get_count_students(self):
         return self.Count_students
 
+    # Функция присваивания значения тегу 'Count_students'
     @get_count_students.setter
     def get_count_students(self, value):
         self.Count_students = value
