4b550476a8291c4d1d73d7b1531e0bc954faff21
Ivan-khm <55186708+Ivan-khm@users.noreply.github.com>
2019-12-07 03:39:23 +0800

    Update Create_xml.py

--- Lab_13/Lvl_13-2/Create_xml.py
+++ Lab_13/Lvl_13-2/Create_xml.py
@@ -6,39 +6,34 @@ class CreateXml:
     def create_xml(self, mas):
         doc = minidom.Document()
 
-        # Создание основного тега 'Timetable'
+        # Создание основного тега 'soap:Envelope'
         root = doc.createElement('soap:Envelope')
         root.setAttribute('xmlns:soap', 'http://example.schemas.xmlsoap.org/soap/envelope/')
         doc.appendChild(root)
 
-        # Создание тега 'Subject'
+        # Создание тега 'Body'
         Body = doc.createElement('soap:Body')
-        # Добавление атрибута 'name_sub' и присвоение ему значения 'KPO'
         root.appendChild(Body)
 
-        # Создание подтега 'Day' в тег 'Subject'
+        # Создание подтега в тег 'productID'
         productID = doc.createElement(mas[0])
-        # Добавление текста 'Saturday' в тег 'Day'
         d = doc.createTextNode('12345')
         productID.appendChild(d)
         Body.appendChild(productID)
 
-        # Создание подтега 'Day' в тег 'Subject'
+        # Создание подтега в тег 'Year'
         Year = doc.createElement(mas[1])
-        # Добавление текста 'Saturday' в тег 'Day'
         w = doc.createTextNode('2019')
         Year.appendChild(w)
         Body.appendChild(Year)
 
-        # Создание подтега 'Day' в тег 'Subject'
+        # Создание подтега в тег 'Month'
         Month = doc.createElement(mas[2])
-        # Добавление текста 'Saturday' в тег 'Day'
         e = doc.createTextNode('July')
         Month.appendChild(e)
         Body.appendChild(Month)
 
         xml_str = doc.toprettyxml(indent="  ")
 
-        # print(xml_str)
         with open("created_xml.xml", "w") as f:
             f.write(xml_str)
